736518bd4bd71830e07e956a30bf7291bc35d7df
liaojiacan <liaojiacan@qq.com>
2019-01-03 20:54:51 +0800

    transfer project

--- spring-i18n-support/src/test/java/com/github/liaojiacan/AbstractTest.java
+++ spring-i18n-support/src/test/java/com/github/liaojiacan/AbstractTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * @author zhengweibao
+ * @author liaojiacan
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { BaseConfig.class })
